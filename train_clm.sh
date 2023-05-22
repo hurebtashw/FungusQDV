eb18271a44fd92e4308419287dcbe072c65773f7
hurebtashw <104347815+hurebtashw@users.noreply.github.com>
2023-05-22 18:55:01 +0300

    Update train_clm.sh

--- train_clm.sh
+++ train_clm.sh
@@ -5,7 +5,6 @@ WORLD_SIZE=8 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 --
     --data_path './data/train_clm_data.json' \
     --output_dir './outputs/train-clm' \
     --batch_size 128 \
-    --micro_batch_size 8 \
     --num_epochs 1 \
     --learning_rate 0.0003 \
     --cutoff_len 1024 \
